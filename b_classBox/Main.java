1791d2c7cd2ca323856e96eb0b5f4ea295d8a6a6
PlamenNeykov <99987548+PlamenNeykov@users.noreply.github.com>
2023-03-05 08:28:50 +0200

    Encapsulation
    You are given a geometric figure Box with fields length, width, and height. Model a class Box that can be instantiated by the same three parameters. Expose to the outside world only methods for its surface area, lateral surface area, and its volume

---
+++ b_classBox/Main.java
@@ -0,0 +1,19 @@
+package b_classBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double l = Double.parseDouble(scanner.nextLine());
+        double w = Double.parseDouble(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
+
+        Box box = new Box(l,w,h);
+
+        System.out.printf("Surface Area - %.2f%n",box.calculateSurfaceArea());
+        System.out.printf("Lateral Surface Area - %.2f%n",box.calculateLateralSurfaceArea());
+        System.out.printf("Volume – %.2f",box.calculateVolume());
+
+    }
+}
